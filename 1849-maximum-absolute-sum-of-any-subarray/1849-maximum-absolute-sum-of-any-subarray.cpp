f545c6525dcdb6e11e400571998b462f80ed92d7
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-02-26 02:54:02 +0200

    Time: 0 ms (100%), Space: 45.2 MB (20.65%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int sum=0,mn=INT_MAX,mx=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            mn=min(mn,sum);
+            mx=max(mx,sum);
+            sum+=nums[i];
+        }
+            mn=min(mn,sum);
+            mx=max(mx,sum);
+        return abs(mn-mx);
+    }
+};
