415e46ab2a383578ab1160fa525c370ae41a7504
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-03 02:15:08 +0200

    Time: 3 ms (88.52%), Space: 127.5 MB (96.86%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        queue<int>q0,q1,q2;
-        int n=nums.size();
+        int n=nums.size(),idx=0,cnt=0;
+        vector<int>ans(n);
         for(int i=0;i<n;i++){
-            if(nums[i]<pivot)q0.push(nums[i]);
-            else if(nums[i]>pivot)q2.push(nums[i]);
-            else q1.push(nums[i]);
+            if(nums[i]<pivot)ans[idx++]=nums[i];
+            if(nums[i]==pivot)cnt++;
         }
-        nums.clear();
-        while(!q0.empty())nums.push_back(q0.front()),q0.pop();
-        while(!q1.empty())nums.push_back(q1.front()),q1.pop();
-        while(!q2.empty())nums.push_back(q2.front()),q2.pop();
-        return nums;
+        while(cnt--)ans[idx++]=pivot;
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot)ans[idx++]=nums[i];
+        }
+        return ans;
     }
 };
