dbb105c269ff31a0e9c222017d3e2a7320a38fd4
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-05 04:24:15 +0200

    Time: 0 ms (100%), Space: 7.9 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans=1;
-        for(long long i=2;i<=n;i++){
-            ans+=(i-1)*4LL;
-        }
-        return ans;
+        long long ans=n;
+        return ans*(ans-1)*2LL+1;
     }
 };
