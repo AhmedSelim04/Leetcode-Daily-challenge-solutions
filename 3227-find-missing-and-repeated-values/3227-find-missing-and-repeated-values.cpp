7ae6ba7a8718988ef2671569d599dfe264eac51c
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-06 15:29:39 +0200

    Time: 0 ms (100%), Space: 26.8 MB (74.89%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -3,19 +3,20 @@ public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid){
         int n=grid.size();
         vector<int>f(n*n+1,0);
-        long long sum=0;
+        int a=0;
         vector<int>ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(!f[grid[i][j]]){
-                    f[grid[i][j]]++;
-                    sum+=grid[i][j];
-                }
-                else ans.push_back(grid[i][j]);
+                if(f[grid[i][j]]==1)
+                    a=grid[i][j];
+                f[grid[i][j]]++;
             }
         }
-        n*=n;
-        ans.push_back(n*(n+1)/2-sum);
-        return ans;
+        for(int i=1;i<=n*n;i++){
+            if(!f[i]){
+                return {a,i};
+            }
+        }
+        return {-1,-1};
     }
 };
