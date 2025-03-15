75c22329ea4779e5b88109f19848b65f5fec546d
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-15 03:36:47 +0200

    Time: 19 ms (81.98%), Space: 60.8 MB (94.16%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-        int n=nums.size(),s=0,e=*max_element(nums.begin(),nums.end()),ans=-1;
+        int n=nums.size(),s=0,e=0,ans=-1;
+        for(int i=0;i<n;i++)e=max(e,nums[i]);
         while(s<=e){
             int mid=(s+e)/2,cnt=0;
             for(int i=0;i<n;i++){
