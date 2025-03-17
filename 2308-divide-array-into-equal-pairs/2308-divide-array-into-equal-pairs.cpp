8214b58c160580d4affdcf70dfec299d93f7193f
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-17 04:25:01 +0200

    Time: 0 ms (100%), Space: 17 MB (66.52%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        unordered_map<int,int>f;
+        vector<int>f(501,0);
         for(auto i:nums)f[i]++;
         for(auto i:nums)if(f[i]&1)return false;
         return true;
