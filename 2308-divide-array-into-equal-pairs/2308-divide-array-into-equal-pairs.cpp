98747888adc639fd41bdede3f96024628966cc70
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-17 04:26:08 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        vector<int>f(501,0);
-        for(auto i:nums)f[i]++;
-        for(auto i:nums)if(f[i]&1)return false;
-        return true;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+            if(nums[i]!=nums[i+1])return false;
+        }
     }
 };
