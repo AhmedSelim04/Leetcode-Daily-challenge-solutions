f85c87c720eb27f01ba3fa2dbb1af2971182cc46
Ahmed Selim <148998681+AhmedSelim04@users.noreply.github.com>
2025-03-19 03:40:04 +0200

    Time: 19 ms (8.79%), Space: 140.1 MB (15.99%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int cnt=0,ans=0;
-        for(int i=0;i<2;i++)cnt+=nums[i]==0;
-        for(int i=2;i<nums.size();i++){
-            cnt+=nums[i]==0;
-            if(cnt){
-                if(nums[i-2]==0){
-                    nums[i-2]=1-nums[i-2];
-                    nums[i-1]=1-nums[i-1];
-                    nums[i]=1-nums[i];
-                    cnt=3-cnt;
-                    ans++;
-                }
+        int cnt=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                if(i+1>=n||i+2>=n)return -1;
+                cnt++;
+                nums[i+1]=1-nums[i+1];
+                nums[i+2]=1-nums[i+2];
             }
-            cnt-=nums[i-2]==0;
         }
-        for(int i=0;i<nums.size();i++)if(nums[i]==0)return -1;
-        return ans;
+        return cnt;
     }
 };
